3624d3d7b7e85cd4c4ab48041ed68d720d50571d
YoshikiOsawa <y_oosawa@sancom.co.jp>
2016-06-02 18:40:19 +0900

    Revert "test"
    This reverts commit d2c5abfe1f61157c6859003f5ddf40ff3acad470.

--- HiraganaHenkan_CSharp/HiraganaHenkan_CSharp/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace HiraganaHenkan_CSharp.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- HiraganaHenkan_CSharp/HiraganaHenkan_CSharp/Class_Library/Message.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Message
-{
-    public readonly string minyuuryokuError = "入力してください。";
-    public readonly string notHankakuEisuuError = "半角英数字で入力してください。";
-    public readonly string mojisuuOverError = "文字以内で入力してください。";
-
-}
